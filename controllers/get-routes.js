d2ea4175f0036e17243097fbab3cbb61befd4d5d
Cong Thanh Pham <chrispham211@gmail.com>
2023-02-10 14:28:45 -0600

    structure for mindmap stuff

--- controllers/get-routes.js
+++ controllers/get-routes.js
@@ -2,31 +2,31 @@ const router = require('express').Router();
 const { User, Team, Project } = require('../models');
 //connection signal
 router.get('/', (req, res) => {
-  res.json({message: 'reached controllers/getRoutes'})
-})
+  res.json({ message: 'reached controllers/getRoutes' });
+});
 
 //get home
 router.get('/', (req, res) => {
-  res.json({message: 'reached home'})
-})
+  res.json({ message: 'reached home' });
+});
 
 //get team
 router.get('/team', (req, res) => {
-  res.json({message: 'reached controllers/getRoutes'})
-})
+  res.json({ message: 'reached controllers/getRoutes' });
+});
 
 //get project
 router.get('/project', (req, res) => {
-  res.json({message: 'reached controllers/getRoutes'})
-})
+  res.json({ message: 'reached controllers/getRoutes' });
+});
 
 //get resources
 router.get('/resources', (req, res) => {
-  res.json({message: 'reached controllers/getRoutes'})
-})
+  res.json({ message: 'reached controllers/getRoutes' });
+});
 
-
-
-
+router.get('/mindmap', (req, res) => {
+  res.render('mindmap');
+});
 
 module.exports = router;
