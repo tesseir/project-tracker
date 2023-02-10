02f19afe9c76d23fdc1c52553081840a632882c1
maxwell walters <118125584+4therealm@users.noreply.github.com>
2023-02-10 13:29:49 -0600

    basic model structure comeplete

---
+++ models/User.js
@@ -0,0 +1,56 @@
+const { Model, DataTypes } = require("sequelize");
+const bcrypt = require("bcrypt");
+const sequelize = require("../config/connection");
+
+class User extends Model {
+  checkPassword(loginPw) {
+    console.log(` @check password (input) ${loginPw}`);
+    console.log(` @check password (existing) ${this.password}`);
+
+    return bcrypt.compareSync(loginPw, this.password);
+  }
+}
+
+User.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true,
+      },
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [8],
+      },
+    },
+  },
+  {
+    hooks: {
+      async beforeCreate(newUserData) {
+        console.log(` before create hook, unhashed ${newUserData}`);
+        newUserData.password = await bcrypt.hash(newUserData.password, 10);
+        console.log(` before create hook, hashed password ${newUserData}`);
+        return newUserData;
+      },
+    },
+    sequelize,
+    timestamps: true,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "User",
+  }
+);
+console.log(`User model connection =`)
+console.log( User=== sequelize.models.User)
+module.exports = User;
